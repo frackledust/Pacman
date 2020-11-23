410c0aefc6f681e487104f122a37fbe9ce75335e
Pavlína <pavli2001@seznam.cz>
2020-11-23 17:17:03 +0100

    update

--- zelva.c
+++ zelva.c
@@ -2,38 +2,58 @@
 #include <stdlib.h>
 #include <time.h>
 
-void prekazky(char * mrizka, int mrizka_size, int pocet);
+#define BORDER_SIGN '#'
+#define DRAW_SIGN 'o'
+
+typedef struct Grid
+{
+    char *items;
+    int rows;
+    int cols;
+
+} Grid;
+
+typedef struct Position
+{
+    int row;
+    int col;
+
+} Position;
+
+void Array_random_change(char* array, int array_size, int num, char symbol);
 
 int main()
 {
-    int rows, cols;
+    Grid grid;
 
     //Načte ze vstupu dvě čísla oddělená mezerou, rows a cols.
-    if (scanf("%d %d", &rows, &cols) != 2)
+    if (scanf("%d %d", &grid.rows, &grid.cols) != 2)
     {
         printf("Error. Ukončuji program. \n");
         return 0;
     }
 
     //Iniciace mřížky
-    char *mrizka = (char *)malloc(rows * cols * sizeof(char));
+    grid.items = (char *)malloc(grid.rows * grid.cols * sizeof(char));
 
-    for (int i = 0; i < rows * cols; i++)
+    for (int i = 0; i < grid.rows * grid.cols; i++)
     {
-        mrizka[i] = '.';
+        grid.items[i] = '.';
     }
 
     //Iniciace želvy na pozici (0, 0)
-    int zelva, zelva_r = 0, zelva_c = 0;
+    Position turtle;
+    turtle.row = 0;
+    turtle.col = 0;
 
     //Generátor 3 náhodných překážek
-    //prekazky(mrizka, rows*cols, 3);
+    Array_random_change(grid.items, grid.rows*grid.cols, 3, BORDER_SIGN);
 
     //Přepis mřížky
     char pokyn;
     do
     {
-        zelva = zelva_r * cols + zelva_c;
+        int position = turtle.row * grid.cols + turtle.col;
 
         //Načtení pokynu
         if (scanf(" %c", &pokyn) != 1)
@@ -42,30 +62,34 @@ int main()
             return 0;
         }
 
-        int w, h;   // délka a výška obdelníku
+        int w, h; //délka a šířka obdelníku
 
         switch (pokyn)
         {
         //Pohyb
         case '<':
-            if (zelva_c != 0 && mrizka[zelva-1]!='#') zelva_c--;
+            if (turtle.col != 0 && grid.items[position - 1] != BORDER_SIGN)
+                turtle.col--;
             break;
 
         case '>':
-            if (zelva_c != (cols - 1) && mrizka[zelva+1]!='#') zelva_c++;
+            if (turtle.col != (grid.cols - 1) && grid.items[position + 1] != BORDER_SIGN)
+                turtle.col++;
             break;
 
         case '^':
-            if (zelva_r != 0 && mrizka[zelva-cols]!='#') zelva_r--;
+            if (turtle.row != 0 && grid.items[position - grid.cols] != BORDER_SIGN)
+                turtle.row--;
             break;
-            
+
         case 'v':
-            if (zelva_r != (rows - 1) && mrizka[zelva+cols]!='#') zelva_r++;
+            if (turtle.row != (grid.rows - 1) && grid.items[position + grid.cols] != BORDER_SIGN)
+                turtle.row++;
             break;
 
         //Kreslení bodu
         case 'o':
-            mrizka[zelva] = 'o';
+            grid.items[position] = DRAW_SIGN;
             break;
 
         //Kreslení obdelníku
@@ -78,20 +102,20 @@ int main()
                 return 0;
             }
 
-            if ((zelva_c + w) <= cols && (zelva_r + h) <= rows)
+            if ((turtle.col + w) <= grid.cols && (turtle.row + h) <= grid.rows)
             {
                 //Kreslí vodorovné čáry
                 for (int c = 0; c < w; c++)
                 {
-                    mrizka[zelva_r * cols + zelva_c + c] = 'o';
-                    mrizka[(zelva_r + h - 1) * cols + zelva_c + c] = 'o';
+                    grid.items[turtle.row * grid.cols + turtle.col + c] = DRAW_SIGN;
+                    grid.items[(turtle.row + h - 1) * grid.cols + turtle.col + c] = DRAW_SIGN;
                 }
 
                 //kreslí svislé čáry (bez překrývání)
                 for (int r = 1; r < h - 1; r++)
                 {
-                    mrizka[(zelva_r + r) * cols + zelva_c] = 'o';
-                    mrizka[(zelva_r + r) * cols + zelva_c + w - 1] = 'o';
+                    grid.items[(turtle.row + r) * grid.cols + turtle.col] = DRAW_SIGN;
+                    grid.items[(turtle.row + r) * grid.cols + turtle.col + w - 1] = DRAW_SIGN;
                 }
             }
 
@@ -101,27 +125,27 @@ int main()
     } while (pokyn != 'x');
 
     //Tisk mřížky
-    for (int r = 0; r < rows; r++)
+    for (int r = 0; r < grid.rows; r++)
     {
-        for (int c = 0; c < cols; c++)
+        for (int c = 0; c < grid.cols; c++)
         {
-            printf("%c", mrizka[r * cols + c]);
+            printf("%c", grid.items[r * grid.cols + c]);
         }
         printf("\n");
     }
 
-    free(mrizka);
+    free(grid.items);
     return 0;
 }
 
-void prekazky(char * mrizka, int mrizka_size, int pocet)
+void Array_random_change(char* array, int array_size, int num, char symbol)
 {
     srand(time(NULL));
     int random_spot;
 
-    for (int i = 0; i < pocet; i++)
+    for (int i = 0; i < num; i++)
     {
-        random_spot = rand() % (mrizka_size);
-        mrizka[random_spot]= '#';
+        random_spot = rand() % (array_size);
+        array[random_spot] = symbol;
     }
 }
