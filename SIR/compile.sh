bbd677320ebd8be7c72ca5d050aae962be6b678c
Pav <72024087+frackledust@users.noreply.github.com>
2021-06-10 19:16:14 +0200

    Delete SIR directory

--- SIR/compile.sh
+++
@@ -1,4 +0,0 @@
-gcc -fsanitize=address -g -c sir.c -o sir.o
-gcc -fsanitize=address -g -c svg.c -o svg.o
-gcc -fsanitize=address -g -c main.c -o main.o
-gcc sir.o svg.o main.o -fsanitize=address -g -o program
--- SIR/main.h
+++
@@ -1,5 +0,0 @@
-#include "common.h"
-
-int Data_create(int argc, char **argv, Data *data);
-void sir_allocate(Data *data);
-int sir_draw(Data data, char *link, int width, int height, int shift);
